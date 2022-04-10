f10c7e929132581e07de6cf88a7392075c7b84a0
Sveta <svet.lukyantseva@gmail.com>
2022-04-10 22:48:18 +0300

    fix: tests

--- angular-basics/src/app/app.component.spec.ts
+++ angular-basics/src/app/app.component.spec.ts
@@ -19,13 +19,13 @@ describe('AppComponent', () => {
   it(`should have as title 'angular-basics'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-basics');
+    expect(app.title).toEqual('Angular Task');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-basics app is running!');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Angular Task');
   });
 });
